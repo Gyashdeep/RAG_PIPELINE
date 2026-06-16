c7536dc7ef8375c07974619cf24e1ad910fb11ce
Gyashdeep <168805806+Gyashdeep@users.noreply.github.com>
2026-06-16 13:26:08 +0530

    main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.retrieval import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 st.set_page_config(page_title="Enterprise AI Data Factory", layout="wide")
 st.title("🤖 Enterprise AI Data Factory")
